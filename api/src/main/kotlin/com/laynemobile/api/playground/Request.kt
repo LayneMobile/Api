973ebfd655606d5a29ef33e6132b64f14687182a
Layne Penney <layne@laynemobile.com>
2016-09-24 22:17:50 -0500

    trying something

--- api/src/main/kotlin/com/laynemobile/api/playground/Request.kt
+++ api/src/main/kotlin/com/laynemobile/api/playground/Request.kt
@@ -16,20 +16,57 @@
 
 package com.laynemobile.api.playground
 
-//interface RequestSource<in S : Any> {
-//    fun subscribe(subscriber: S)
-//}
-//
-//sealed class Request<T : Any, in S : Any> {
-//
-//    abstract fun subscribe(subscriber: S)
-//
-//    class RxSingle<T : Any>(
-//            val delegate: Single<T>
-//    ) : Request<T, SingleObserver<in T>>(), SingleSource<T> {
-//        override fun subscribe(subscriber: SingleObserver<in T>) {
-//            delegate.subscribe(subscriber)
-//        }
-//    }
-//}
+import io.reactivex.*
+import org.reactivestreams.Publisher
+import org.reactivestreams.Subscriber
+
+interface RequestSource<in R : Any> {
+    fun subscribe(receiver: R)
+}
+
+interface SingleRequest<T : Any> : RequestSource<SingleObserver<in T>>, SingleSource<T> {
+    override fun subscribe(receiver: SingleObserver<in T>)
+}
+
+interface ObservableRequest<T : Any> : RequestSource<Observer<in T>>, ObservableSource<T> {
+    override fun subscribe(receiver: Observer<in T>)
+}
+
+interface FlowableRequest<T : Any> : RequestSource<Subscriber<in T>>, Publisher<T> {
+    override fun subscribe(receiver: Subscriber<in T>)
+}
+
+sealed class Request<T : Any> {
+    abstract val delegate: Any
+
+    class AsSingle<T : Any>(
+            override val delegate: Single<T>
+    ) : Request<T>(), SingleRequest<T> {
+        override fun subscribe(receiver: SingleObserver<in T>) {
+            delegate.subscribe(receiver)
+        }
+    }
+
+    class AsObservable<T : Any>(
+            override val delegate: Observable<T>
+    ) : Request<T>(), ObservableRequest<T> {
+        override fun subscribe(receiver: Observer<in T>) {
+            delegate.subscribe(receiver)
+        }
+    }
+
+    class AsFlowable<T : Any>(
+            override val delegate: Flowable<T>
+    ) : Request<T>(), FlowableRequest<T> {
+        override fun subscribe(receiver: Subscriber<in T>) {
+            delegate.subscribe(receiver)
+        }
+    }
+}
+
+fun <T : Any, R : Any> Request<T>.map(mapper: (T) -> R): Request<R> = when (this) {
+    is Request.AsSingle -> Request.AsSingle(delegate.map(mapper))
+    is Request.AsObservable -> Request.AsObservable(delegate.map(mapper))
+    is Request.AsFlowable -> Request.AsFlowable(delegate.map(mapper))
+}
 
